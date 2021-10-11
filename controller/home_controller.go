ab58ed685ebb4cdd2e727cfcfead0f3868bc905d
Charent <charent_chen@163.com>
2021-10-11 22:58:29 +0800

    调整数据库

--- controller/home_controller.go
+++ controller/home_controller.go
@@ -25,16 +25,28 @@ func (h *HomeController)GetHomeArticles(c *gin.Context) {
 			"param": page,
 			})
 		c.Abort()
+		return
 	}
 
 	nPage, latestArticles := ArticleService.GetHomeArticles(page)
 
+	// 查到的文章为0，但是页数不为0（数据库中有文章），那就是请求的页数错了
 	if latestArticles == nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"code": http.StatusBadRequest,
-			"message": "页数错误",
-			"page": page,
-		})
+		if nPage != 0 {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"code": http.StatusBadRequest,
+				"message": "页数错误",
+				"page": page,
+			})
+			c.Abort()
+			return
+		} else {
+			// 查到的文章为0，而且文章总数也为0
+			// 构造一个空数组，防止返回null
+			var temp = make([]service.HomeArticleJson, 0)
+			latestArticles = &temp
+		}
+
 	}
 
 	c.JSON(http.StatusOK, gin.H{
